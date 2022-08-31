9cd16737e651f1d5efcd29d4e1ae307df1e57d36
Cristian Poputea <poputeacristi@gmail.com>
2022-09-01 00:03:55 +0300

    work

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
