f65002cd42edf3e3c3f1d38bdfd942e36b97a1a0
Cristian Poputea <poputeacristi@gmail.com>
2022-09-01 00:01:19 +0300

    chore: first module

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+  res.render('index', {title: 'Index' })
+})
+
+app.listen(3000, ()=>{
+  console.log('PS Project Running on port 3000!');
+})
